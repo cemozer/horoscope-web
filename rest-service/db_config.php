d23344d23eb271f25e48a1a61a6beb6db5c71721
Cem Özer <cem.ozer.cse@gmail.com>
2015-04-06 00:06:16 +0300

    Update db_config.php

--- rest-service/db_config.php
+++ rest-service/db_config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('PG_CON_STR', "host=ec2-184-73-165-193.compute-1.amazonaws.com port=5432 dbname=d9k07c0jldc8pq user=fcfgjhjyfjccpe password=zYvtifURsoFyvAsaXAc2LsjIka");
+define('PG_CON_STR', "ec2-107-21-93-97.compute-1.amazonaws.com port=5432 dbname=db9rdak8b9ttv1 user=lhbbonstvplgjk password=bXzqP6kgB3cvKC5-H8mQ0KS9AV");
 
 
 ?>
